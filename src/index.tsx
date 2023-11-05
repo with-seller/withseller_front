91f04548761ec87d3ab9867360456295b59631bd
scnoh <stc9606@naver.com>
2023-11-05 21:05:44 +0900

    [fix] index css 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css';
 import { ThemeProvider } from '@emotion/react';
 
 import theme from './styles/theme';
