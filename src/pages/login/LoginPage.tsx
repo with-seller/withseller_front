9d56386a454204e2e7dd39a84ff5433952068b99
scnoh <stc9606@naver.com>
2023-11-08 23:14:15 +0900

    [fix] login page css 수정

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -2,54 +2,22 @@ import {loginBody, loginCard} from "./LoginPage.styles";
 
 function LoginPage() {
     return (
-        <div className="bg-slate-50 min-h-screen flex">
-            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 ">
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        로그인
-                    </h2>
-                </div>
-
-                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-md px-6 py-12 shadow-lg rounded-lg bg-white">
+        <div className="bg-gradient-to-r from-purple-500 to-indigo-500 min-h-screen flex items-center justify-center">
+            <div className="bg-white w-full max-w-md rounded-lg shadow-lg">
+                <div className="px-10 py-8">
                     <form className="space-y-6">
                         <div>
-                            <label
-                                htmlFor="email"
-                                className="block text-sm font-medium leading-6 text-gray-900"
-                            >
-                                이메일
-                            </label>
-                            <div className="mt-2">
-                                <input
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    autoComplete="email"
-                                    required
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
+                            <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1"> 아이디 </label>
+                            <input id="email" name="email" type="email" autoComplete="email" required className="block w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent"/>
                         </div>
-
                         <div>
-                            <div className="flex items-center justify-between">
-                                <label
-                                    htmlFor="password"
-                                    className="block text-sm font-medium leading-6 text-gray-900"
-                                >
-                                    비밀번호
-                                </label>
-                            </div>
-                            <div className="mt-2">
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    autoComplete="current-password"
-                                    required
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
+                            <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1"> 비밀번호 </label>
+                            <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent"/>
+                        </div>
+                        <div>
+                            <button type="submit" className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                로그인
+                            </button>
                         </div>
                     </form>
                 </div>
