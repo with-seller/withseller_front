5adcdeb04ae46bb3829d02823a3a372642ead1dd
scnoh <stc9606@naver.com>
2023-11-24 23:24:28 +0900

    [feat] login api module 추가

---
+++ src/api/login.ts
@@ -0,0 +1,21 @@
+import api from './';
+
+const loginAPI = {
+    endPoint: {
+        login: '/api/auth/login',
+        signUp: '/api/auth/sign-up'
+    },
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+
+    login: async (id: string, password: string) => {
+        const { data } = await api.post(loginAPI.endPoint.login, {
+            id, password
+        });
+
+        return data;
+    }
+
+}
