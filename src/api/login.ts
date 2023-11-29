73e4640cbd737e7e0cb1a6999b6a72f40461d6c0
scnoh <stc9606@naver.com>
2023-11-29 21:26:13 +0900

    [fix] login module 수정

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,21 +1,21 @@
 import api from './';
 
 const loginAPI = {
-    endPoint: {
-        login: '/api/auth/login',
-        signUp: '/api/auth/sign-up'
-    },
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    },
+  endPoint: {
+    login: '/api/auth/login',
+    signUp: '/api/auth/sign-up',
+  },
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
 
-    login: async (id: string, password: string) => {
-        const { data } = await api.post(loginAPI.endPoint.login, {
-            id, password
-        });
+  login: async (id: string, password: string) => {
+    const { data } = await api.post(loginAPI.endPoint.login, {
+      id,
+      password,
+    });
 
-        return data;
-    }
-
-}
+    return data;
+  },
+};
